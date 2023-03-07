ad9e6704c28a5d961eefb7269439c8a9f8423fd4
alishervoitov <voitovalisher020@gmail.com>
2023-03-07 23:14:32 +0500

    swagger

--- registration_api/urls.py
+++ registration_api/urls.py
@@ -13,10 +13,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static, settings
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Registartion API",
+      default_version='v1',
+      description="Swagger docs for REST API",
+      contact=openapi.Contact("Registration <info@Registarer.uz>"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/user/', include('register.urls')),
+    path('api/token-obtain-pair-view/', TokenObtainPairView.as_view(), name='tokenobtainpairview'),
+    path('api/refresh-token/', TokenRefreshView.as_view(), name='refreshtoken'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/api.json/', schema_view.without_ui( cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
