ef9122a97ac330fa494a203e763f7c5266a12a5e
alishervoitov <voitovalisher020@gmail.com>
2023-04-04 13:23:54 +0500

    .

--- registration_api/settings.py
+++ registration_api/settings.py
@@ -146,8 +146,6 @@ AUTH_USER_MODEL = 'register.CustomerUser'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'voitovalisher020@gmail.com'
-EMAIL_HOST_PASSWORD = 'leqwwndwdkodctcn'
 EMAIL_USE_TLS = True
 
 # Default primary key field type
